1590282e9d9bc60d11009e98618242a3d52e16f9
DnlVldz-git <61020584+DnlVldz-git@users.noreply.github.com>
2022-02-17 18:23:22 -0600

    databasea

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const s3 = new aws.S3({
     //apiVersion: '2006-03-01',
     accessKeyId: accessKeyId,
     secretAccessKey: secretAccesKey,
-    region: region,    
+    region: region,
     signatureVersion: 'v4'
 });
 
 //require('./models/associations.models');
 
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
 const app = express();
@@ -67,10 +67,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.post('/mexico-amparame/s3Url', upload.single('foto'), async (req, res) => {    
+app.post('/mexico-amparame/s3Url', upload.single('foto'), async (req, res) => {
     const stream = fs.createReadStream(req.file.path);
 
-    const ext = path.extname(req.file.originalname).toLowerCase();    
+    const ext = path.extname(req.file.originalname).toLowerCase();
 
     let fileType = "";
 
@@ -79,7 +79,7 @@ app.post('/mexico-amparame/s3Url', upload.single('foto'), async (req, res) => {
     } else if (ext == ".jpg" || ext == ".jpeg") {
         fileType = "image/jpg";
     } else {
-        res.send({data: "error"});
+        res.send({ data: "error" });
     }
 
     stream.on("error", function (err) {
@@ -103,67 +103,67 @@ app.post('/mexico-amparame/s3Url', upload.single('foto'), async (req, res) => {
 
 app.get('/mexico-amparame/s3Url2/:key', async (req, res) => {
     const key = req.params.key;
-    const imagen = await getImagen(key);            
+    const imagen = await getImagen(key);
     res.send(imagen);
 });
 
 app.get('/mexico-amparame/beneficiarios', async (req, res) => {
-    try{
+    try {
         fs.readFile('datos.json', 'utf-8', (err, data) => {
             if (err) {
-                res.send({datos: 'error'});
+                res.send({ datos: 'error' });
                 return;
             }
-        
+
             // parse JSON object
-            const datos = JSON.parse(data.toString());                
+            const datos = JSON.parse(data.toString());
             res.send(datos);
         });
-    }catch(err){
-        
+    } catch (err) {
+
         console.log("no hay json de beneficiarios")
-    }    
+    }
 });
 
 app.post('/mexico-amparame/beneficiarios', async (req, res) => {
     const data = req.body;
     const datos = JSON.stringify(data);
-    try{
+    try {
         fs.writeFile('datos.json', datos, (err) => {
             if (err) {
-                res.send({datos: 'error'});
+                res.send({ datos: 'error' });
                 return;
             }
             console.log("datos guardados.");
         });
-    }catch(err){
+    } catch (err) {
         console.log(err)
-    }    
+    }
 });
 
 app.delete('/mexico-amparame/s3Url3/:key', async (req, res) => {
     const key = req.params.key;
-    const data = await deleteImagen(key);        
-    res.send(data)    
+    const data = await deleteImagen(key);
+    res.send(data)
 });
 
 const PORT = process.env.PORT || 0000;
 app.listen(PORT, () => {
-    console.log(`Server esta ejecutandose en puerto ${PORT}.`);
+    console.log(`Serve esta ejecutandose en puerto ${PORT}.`);
 });
 
 //elimina todos los datos de las tablas y las vuelve a crear
-const sync = (process.env.MA_DB_SYNC === 'true'); 
+const sync = (process.env.MA_DB_SYNC === 'true');
 
 const db = require("./models");
 
 db.sequelize.sync({ alter: sync }).then(() => {
-    if(sync) {
+    if (sync) {
         console.log("Sincronizar db");
-    }else{
+    } else {
         console.log("No se harán cambios a la db");
     }
-    
+
 });
 
 require("./routes/noticia.routes.js")(app);
